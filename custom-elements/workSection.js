af5fc71fb52256bbb7eabe4891b64f9fa62aeb71
jt-papa <jtpapa10@gmail.com>
2022-09-22 17:46:02 -0500

    hw18 push

---
+++ custom-elements/workSection.js
@@ -0,0 +1,29 @@
+'use strict'
+
+const template = document.createElement('template')
+
+template.setAttribute() = 
+
+class workSection extends HTMLElement {
+    constructor() {
+        super()
+        this.attachShadow({mode:'open'})
+        this.shadowRoot.appendChild(template.content.cloneNode(true))
+    }
+}
+
+function attributeChangeCallback(name, oldValue, newValue) {
+    const placeholder = 1
+}
+
+export default workSection
+
+ 
+        
+
+
+
+
+
+
+
---
+++ index.js
@@ -0,0 +1,3 @@
+import workSection from './custom-elements/workSection.js'
+
+window.customElements.define('custom-elements', Work)
